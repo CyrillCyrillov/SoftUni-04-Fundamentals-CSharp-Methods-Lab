6d1325c7c2a6a8dda08d9568f44f8e79fc173b22
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:21:40 +0200

    Add files via upload

---
+++ Task11_Math_Operations/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Task11_Math_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1 = double.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double number2 = double.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = Addition(number1, number2);
+                    break;
+                case "-":
+                    result = Subtraction(number1, number2);
+                    break;
+                case "*":
+                    result = Multiplication(number1, number2);
+                    break;
+                case "/":
+                    if (number2 == 0)
+                        {
+                            Console.WriteLine("Error!");
+                        }  
+                    else
+                        {
+                            result = Division(number1, number2);
+                        }
+                    break;
+
+                default:
+                    break;
+
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static double Addition(double number1, double number2)
+        {
+            double result = number1 + number2;
+            return result;
+        }
+
+        static double Subtraction(double number1, double number2)
+        {
+            double result = number1 - number2;
+            return result;
+        }
+
+        static double Multiplication(double number1, double number2)
+        {
+            double result = number1 * number2;
+            return result;
+        }
+
+        static double Division(double number1, double number2)
+        {
+            double result = number1 / number2;
+            return result;
+        }
+    }
+}
