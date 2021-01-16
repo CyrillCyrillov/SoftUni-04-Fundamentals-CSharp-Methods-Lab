696c59a171d370d5dab7c5f7a3cff60c9401c7e2
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:10:49 +0200

    Add files via upload

---
+++ Task03_Calculations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task03_Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation = Console.ReadLine();
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            
+            Calculation(operation, number1, number2);
+        }
+
+        static void Calculation(string operation, int number1, int number2)
+        {
+            switch (operation)
+            {
+                case "add":
+                    Console.WriteLine(number1 + number2);
+                    break;
+                case "multiply":
+                    Console.WriteLine(number1 * number2);
+                    break;
+                case "subtract":
+                    Console.WriteLine(number1 - number2);
+                    break;
+                case "divide":
+                    Console.WriteLine(number1 / number2);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
