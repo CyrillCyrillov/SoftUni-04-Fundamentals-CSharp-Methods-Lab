9b4a3247f1cfe09efabba4c16595588c701b40f6
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:13:55 +0200

    Add files via upload

---
+++ Task06_Calculate_Rectangle_Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task06_Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = CalculateArea(width, height);
+            Console.WriteLine(area);
+        }
+        static double CalculateArea(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
