8b8112de946c07ae076bed2e548e57af7020be7d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:15:42 +0200

    Add files via upload

---
+++ Task07A_Repeat_String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Task07A_Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            int repeat = int.Parse(Console.ReadLine());
+            string outputString = MultiplateString(inputString, repeat);
+
+            Console.WriteLine(outputString);
+
+            MultiplateString(inputString, repeat);
+        }
+
+        static string MultiplateString(string text, int count)
+        {
+            StringBuilder result = new StringBuilder();
+            string rezult = string.Empty;
+            for (int i = 0; i < count; i++)
+            {
+                result.Append(text);
+            }
+
+            return result.ToString();
+        }
+    }
+}
