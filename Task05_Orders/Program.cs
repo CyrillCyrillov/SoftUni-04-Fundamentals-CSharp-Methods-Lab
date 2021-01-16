e77ee36b6de2f10b75af2c7901b0ea31570a25e6
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:12:58 +0200

    Add files via upload

---
+++ Task05_Orders/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task05_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+
+            CalculateOrder(product, quantity);
+        }
+
+        static void CalculateOrder(string product, double quantity)
+        {
+            switch (product)
+            {
+                case "coffee":
+                    Console.WriteLine($"{quantity * 1.5:f2}");
+                    break;
+                case "water":
+                    Console.WriteLine($"{quantity * 1:f2}");
+                    break;
+                case "coke":
+                    Console.WriteLine($"{quantity * 1.40:f2}");
+                    break;
+                case "snacks":
+                    Console.WriteLine($"{quantity * 2.00:f2}");
+                    break;
+            }
+        }
+    }
+}
