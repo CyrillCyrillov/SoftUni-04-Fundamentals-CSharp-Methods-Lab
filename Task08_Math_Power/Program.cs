56036c43bb3a4c26459906e45bb5e2198f8da1d3
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:16:33 +0200

    Add files via upload

---
+++ Task08_Math_Power/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task08_Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1 = double.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(Gradation(number1, number2));
+        }
+
+        static double Gradation(double basis, int power)
+        {
+            double result = 1;
+            for (int i = 1; i <= power; i++)
+            {
+                result *= basis;
+            }
+            
+            return result;
+        }
+    }
+}
